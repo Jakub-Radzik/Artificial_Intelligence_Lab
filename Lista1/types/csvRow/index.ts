ba67b304f5e961ab502b28dfb925de0a7832e461
Jakub Radzik <jradzik4@gmail.com>
2023-03-12 18:21:24 +0100

    Types for node and csv row

---
+++ Lista1/types/csvRow/index.ts
@@ -0,0 +1,12 @@
+export type CsvRow = {
+  company: string;
+  line: string;
+  departure_time: string;
+  arrival_time: string;
+  start_stop: string;
+  end_stop: string;
+  start_stop_lat: string;
+  start_stop_lon: string;
+  end_stop_lat: string;
+  end_stop_lon: string;
+};
---
+++ Lista1/types/graph/index.ts
@@ -0,0 +1,5 @@
+export type NodeJR = {
+  name: string;
+  lat: number;
+  lon: number;
+};
