e66ff5948dcdac70a598c79dcdad73e5ca5ffe16
Jakub Radzik <jradzik4@gmail.com>
2023-03-13 19:36:34 +0100

    Tests

---
+++ Lista1/__tests__/utils.test.ts
@@ -0,0 +1,26 @@
+import {describe, test, expect} from '@jest/globals';
+import { getMinutesDifference } from '../utils/time';
+
+describe("Utils", () => {
+  describe("getTimeDifference", () => {
+    test("Should return minutes", ()=>{
+      const h1 = "15:21";
+      const h2 = "16:20";
+      const diff = getMinutesDifference(h1, h2);
+      expect(diff).toBe(59);
+    });
+  
+    test("Should throw error", ()=>{
+      const h1 = "15:21";
+      const h2 = "14:20";
+      expect(() => getMinutesDifference(h1, h2)).toThrow();
+    });
+
+    test("Should return zero", ()=>{
+      const h1 = "15:21";
+      const h2 = "15:21";
+      const diff = getMinutesDifference(h1, h2);
+      expect(diff).toBe(0);
+    });
+  });
+});
--- Lista1/utils/time.ts
+++ Lista1/utils/time.ts
@@ -1,2 +1,12 @@
 export const getTimeDifference = (startTime: number) =>
   (new Date().getTime() - startTime) / 1000;
+
+export const getMinutesDifference = (time1: string, time2: string) => {
+    const [hours1, minutes1] = time1.split(':').map(Number);
+    const [hours2, minutes2] = time2.split(':').map(Number);
+  
+    const minutesDifference = (hours2 * 60 + minutes2) - (hours1 * 60 + minutes1);
+
+    if(minutesDifference < 0) throw new Error("Time difference is negative");
+    return minutesDifference;
+  }
