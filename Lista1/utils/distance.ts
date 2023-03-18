b4d2f1ab53b2fe1f950b2a981730eff82d43b507
Jakub Radzik <jradzik4@gmail.com>
2023-03-18 12:37:41 +0100

    distances

--- Lista1/utils/distance.ts
+++ Lista1/utils/distance.ts
@@ -1,3 +1,5 @@
+import { GraphNode } from "../types";
+
 export const manhattanDistance = (
   x1: number,
   y1: number,
@@ -7,7 +9,7 @@ export const manhattanDistance = (
   return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 };
 
-export const euclideanDistance = (
+const euclideanDistance = (
   x1: number,
   y1: number,
   x2: number,
@@ -15,3 +17,23 @@ export const euclideanDistance = (
 ): number => {
   return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 };
+
+export const euclideanDistanceNodes = (
+  node1: GraphNode,
+  node2: GraphNode
+): number => euclideanDistance(
+    node1.longitude,
+    node1.latitude,
+    node2.longitude,
+    node2.latitude
+  );
+
+export const manhattanDistanceNodes = (
+  node1: GraphNode,
+  node2: GraphNode
+): number =>  manhattanDistance(
+    node1.longitude,
+    node1.latitude,
+    node2.longitude,
+    node2.latitude
+  );  
